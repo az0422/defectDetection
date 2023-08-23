8d2a210f659908d83bbf88c8cdaf12e4f16d98de
az0422 <87636192+az0422@users.noreply.github.com>
2023-08-23 11:33:43 +0900

    commit

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export DOCKER_BUILDKIT=1
-docker build -t defect:latest --build-arg uid=${UID} .
+UID=`cat /etc/passwd | grep $USER | awk -F: '{print $4}'`
+docker build -t defect:latest --build-arg uid=$UID .
